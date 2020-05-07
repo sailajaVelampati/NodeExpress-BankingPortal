2849edda3fe1a7860705fa84968d0a2be8881306
sailaja velmapati <sailajasmart@gmail.com>
2020-05-07 16:11:39 +0530

    3.1

--- src/app.js
+++ src/app.js
@@ -8,6 +8,10 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
+const accountData = fs.readFileSync("src/json/accounts.json", {
+  encoding: "utf8",
+});
+const accounts = JSON.parse(accountData);
 app.get("/", (req, res) => {
   res.render("index", { title: "Index" });
 });
