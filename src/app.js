bf71c1d74893a61a9c20f37af53e446483faf778
sailaja velmapati <sailajasmart@gmail.com>
2020-05-08 14:09:51 +0530

    6.12

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const { accounts, users, writeJSON } = require("./data");
+const accountRoutes = require("./routes/accounts");
+const servicesRoutes = require("./routes/services");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
