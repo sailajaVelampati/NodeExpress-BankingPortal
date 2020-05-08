abaf525d20830d0b29941eefed8b8e372606c484
sailaja velmapati <sailajasmart@gmail.com>
2020-05-08 12:04:46 +0530

    5.8

--- src/data.js
+++ src/data.js
@@ -1,20 +1,22 @@
 const fs = require("fs");
 const path = require("path");
 
-// const accounts = JSON.parse(accountData);
-// const userData = fs.readFileSync("src/json/users.json", {
-//   encoding: "utf8",
-// });
+const accountData = fs.readFileSync("src/json/accounts.json", {
+  encoding: "utf8",
+});
+const userData = fs.readFileSync("src/json/users.json", {
+  encoding: "utf8",
+});
 
-// const writeJSON = () => {
-//   const accountsJSON = JSON.stringify(accounts);
-//   fs.writeFileSync(
-//     path.join(__dirname, "json/accounts.json"),
-//     accountsJSON,
-//     "UTF8"
-//   );
-// };
+const writeJSON = () => {
+  const accountsJSON = JSON.stringify(accountData);
+  fs.writeFileSync(
+    path.join(__dirname, "json/accounts.json"),
+    accountsJSON,
+    "UTF8"
+  );
+};
 
-// module.exports.writeJSON = writeJSON;
-// module.exports.accounts = accounts;
-// module.exports.userData = userData;
+module.exports.writeJSON = writeJSON;
+module.exports.accounts = accountData;
+module.exports.users = userData;
